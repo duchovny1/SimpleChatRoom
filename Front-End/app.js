4d56438c70db87fc504b1bc2fb60d46e2d854778
duchovny1 <54773747+duchovny1@users.noreply.github.com>
2020-03-16 22:50:53 +0200

    upload front end

---
+++ Front-End/app.js
@@ -0,0 +1,87 @@
+const appUrl = 'https://localhost:44326/api/';
+
+let currentUsername = null;
+
+function renderMesages(data) {
+    $('#messages').empty();
+   for(let message of data){
+    $('#messages').append
+      ('<div class="message d-flex justify-content-start"><strong>'
+          + message.user 
+          + '</strong>:' 
+          + message.content 
+          + '</div>')
+   }
+}
+
+function loadMessages(){
+     $.get({
+         url: appUrl + 'messages/all',
+         success: function succes(data){
+            renderMesages(data);
+         },
+         error: function error(error){
+           console.log(error)
+         }
+
+     })
+}
+
+function createMessage(){
+     let username = currentUsername;
+     let message = $('#message').val();
+
+     if(username == null){
+         alert('choose username first!');
+         return;
+     }
+     if(message.length === 0){
+         alert('you cannot choose empty message!')
+         return;
+     }
+     
+     console.log(username)
+     console.log(message)
+     $.post({
+        url: appUrl + 'messages/create',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({content: message, user: username}),
+        success: function success(data) {
+            loadMessages();
+        },
+        error: function error(error) {
+            console.log(error);
+        }
+    });
+
+    $('#message').text("");
+
+}
+
+
+function chooseUsername(){
+    let username = $('#username').val();
+
+    if(username.length === 0){
+        alert('you cant choose empty username');
+        return;
+    }
+
+    currentUsername = username
+    $('#username-choice').text(currentUsername);
+    $('#choose-data').hide();
+    $('#reset-data').show();
+
+}
+
+function resetUsername(){
+   currentUsername = null;
+
+   $('#choose-data').show();
+   $('#reset-data').hide();
+}
+
+$('#reset-data').hide();
+loadMessages();
